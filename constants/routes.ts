2e65b66eae472c41d254d25677820cffc38cdafb
xxKeefer <xxkeefer.code@gmail.com>
2022-04-26 22:09:30 +1000

    feat(routes): :sparkles: add routes constant

---
+++ constants/routes.ts
@@ -0,0 +1,6 @@
+export const ROUTES = {
+    HOME: '/',
+    LINKEDIN: 'https://www.linkedin.com/in/xxkeefer/',
+    GITHUB: 'https://github.com/xxKeefer',
+    TWITTER: 'https://twitter.com/xxKeefer',
+}
